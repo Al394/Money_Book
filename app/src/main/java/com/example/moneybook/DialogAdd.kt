d0d08a279c5086905c48f09512240b1a92fe0671
Al3 <ale_sancio@hotmail.com>
2020-06-10 18:17:52 +0200

    elementi della recycleview2

---
+++ app/src/main/java/com/example/moneybook/DialogAdd.kt
@@ -0,0 +1,30 @@
+package com.example.moneybook
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import java.util.zip.Inflater
+
+class DialogAdd : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            // Use the Builder class for convenient dialog construction
+            val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater
+                builder.setView(R.layout.dialog_add_item)
+                .setPositiveButton(R.string.accept,
+                    DialogInterface.OnClickListener { dialog, id ->
+
+                    })
+                .setNegativeButton(R.string.cancel,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User cancelled the dialog
+                    })
+            // Create the AlertDialog object and return it
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
--- app/src/main/java/com/example/moneybook/MainActivity.kt
+++ app/src/main/java/com/example/moneybook/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.moneybook
 
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        add.setOnClickListener(){
+            val sfm = supportFragmentManager
+            val addfr=DialogAdd()
+            addfr.show(sfm,"boh")
+        }
+
+
+
     }
+
+
 }
